8abbc35eb5ff0c9f90b4ad8622cceb47d69cb217
akshad <akshad.agrawal1978@gmail.com>
2023-12-24 23:32:16 +0530

    main website done, content left

--- final project/script.js
+++ final project/script.js
@@ -36,3 +36,52 @@ $(document).ready(function(){
     });
   
   });
+
+  let  numberPercent = document.querySelectorAll('.countbar')
+let getPercent = Array.from(numberPercent)
+
+getPercent.map((items) => {
+    let startCount = 0
+    let progressBar = () => {
+        startCount ++
+        items.innerHTML = `<h3>${startCount}%</h3>`
+        items.style.width = `${startCount}%`
+        if(startCount == items.dataset.percentnumber) {
+            clearInterval(stop)
+        }
+    }
+    let stop = setInterval(() => {
+        progressBar()
+    },25)
+})
+
+function openSlideshow() {
+  document.getElementById('slideshowContainer').style.display = 'block';
+  showImage(currentIndex);
+}
+
+function closeSlideshow() {
+  document.getElementById('slideshowContainer').style.display = 'none';
+}
+
+// JavaScript for slideshow functionality
+let currentIndex = 0;
+const images = document.getElementById('slideshowContent').getElementsByClassName('slideshow-img');
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+function showImage(index) {
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+  }
+
+  images[index].style.display = 'block';
+}
